4192c819d3700d995c05d4c8ceea81f44e5be01f
Lucas Pereira Borges <112784071+lucaspbg@users.noreply.github.com>
2025-12-10 11:53:36 -0300

    Update Supabase key in supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://wdmsfmmzaiodwxjyvdtd.supabase.co';
-const supabaseKey = 'sb_secret_cQ3jdp5lDqEl36pL8mwnKQ_SwLBRguI';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbXNmbW16YWlvZHd4anl2ZHRkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjQxMjU5OCwiZXhwIjoyMDQ3OTg4NTk4fQ.TYIEjC1ngPaFkkorElpYKPIzNZDjVWoO3__sMTG0se0';
 export const supabase = createClient(supabaseUrl, supabaseKey);
