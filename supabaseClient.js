f82566e766d1227c740042a3520ebab8ff92b4f1
Lucas Pereira Borges <112784071+lucaspbg@users.noreply.github.com>
2025-12-10 11:50:25 -0300

    Change Supabase key to secret key
    Updated Supabase key from publishable to secret.

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://wdmsfmmzaiodwxjyvdtd.supabase.co';
-const supabaseKey = 'sb_publishable_vziq1mIU8RURLfALlc0YxA_pno2srr_';
+const supabaseKey = 'sb_secret_cQ3jdp5lDqEl36pL8mwnKQ_SwLBRguI';
 export const supabase = createClient(supabaseUrl, supabaseKey);
