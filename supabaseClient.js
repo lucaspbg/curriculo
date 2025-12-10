b459901f20b58fbc77aabb1678c55ee55dd9fcf5
Lucas Pereira Borges <112784071+lucaspbg@users.noreply.github.com>
2025-12-10 11:48:33 -0300

    Update Supabase key to a publishable key

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://wdmsfmmzaiodwxjyvdtd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbXNmbW16YWlvZHd4anl2ZHRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0MTI1OTgsImV4cCI6MjA0Nzk4ODU5OH0.JzFOFNdqNMMk9yneU97XxGIWMlHLjb3PKHffiorT9y4';
+const supabaseKey = 'sb_publishable_vziq1mIU8RURLfALlc0YxA_pno2srr_';
 export const supabase = createClient(supabaseUrl, supabaseKey);
